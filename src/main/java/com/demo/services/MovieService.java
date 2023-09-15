e427d1259e2aa8529db212f755d989e8d061c56b
Arun Kumar <arun.kumar@iiht.co.in>
2023-09-15 09:15:38 +0530

    MovieService class is complete

--- src/main/java/com/demo/services/MovieService.java
+++ src/main/java/com/demo/services/MovieService.java
@@ -17,23 +17,43 @@ public class MovieService {
 	private MovieRepository repo;
 
 	public List<Movie> getAllMovies(){
-		//...
+		return repo.findAll();
 	}
 
 	public Movie getMovieById(int id) throws MovieException{
-		//...
+		Optional<Movie> optional = repo.findById(id);
+		if(optional.isPresent()){
+			return optional.get();
+		} else {
+			throw new MovieException("Movie with id #"+id+" not found");
+		}
 	}
 	
 	public Movie saveMovie(Movie m){
-		//...
+		return repo.save(m);
 	}
 
 	public void deleteMovie(int id) throws MovieException{
-		//...
+		try{
+			Movie m = getMovieById(id);
+			repo.delete(m);
+		} catch (Exception e){
+			throw new MovieException("Movie with id #"+id+" not found and hence not deleted");
+		}
 	}
 
 	public Movie updateMovie(int id, Movie m) throws MovieException{
-		//...
+		try{
+			Movie movie = getMovieById(id);
+
+			movie.setDirector(m.getDirector());
+			movie.setTitle(m.getTitle());
+			movie.setRating(m.getRating());
+			return repo.save(movie);
+		} catch(MovieException e){
+			throw new MovieException("Movie with id #"+id+" not found and thus could not be updated");
+		}
 	}
 	
+
 }
